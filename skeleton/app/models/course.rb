7a307f9666740d9fbea2bd44e7afd5465ba0b549
Yehudah <jros987@gmail.com>
2020-11-25 15:27:49 -0500

    first

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+    belongs_to :instructor,
+      class_name: :User,
+      foreign_key: :instructor_id,
+      primary_key: :id
+
+      has_one
+
+
 end
+#all assoss. are 2 sides. We don't always need another side for instructer id.
+
+#has_one is for one-to-one relationships. Rare.
